fdae80ac98685e21e6b08936f72cc369d96065d3
wobey96 <wobey96@gmail.com>
2023-11-27 01:59:44 -0700

    interpolation index draw done

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@
 const GLint WIDTH = 800, HEIGHT = 600;
 const float toRadians = 3.14159265f / 180.0f; 
 
-GLuint VBO, VAO, shader, uniformModel;
+GLuint VBO, VAO, IBO, shader, uniformModel;
 
 bool direction = true; 
 float triOffset = 0.0f; 
@@ -34,34 +34,51 @@ static const char* vShader = "                                                \n
                                                                               \n\
 layout (location = 0) in vec3 pos;											  \n\
                                                                               \n\
+out vec4 vCol;                                                                \n\
+                                                                              \n\
 uniform mat4 model;                                                           \n\
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
     gl_Position = model * vec4(pos, 1.0);	                                  \n\
+    vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);                                \n\
 }";
 
 // Fragment Shader
 static const char* fShader = "                                                \n\
 #version 330                                                                  \n\
                                                                               \n\
+in vec4 vCol;                                                                 \n\
+                                                                              \n\
 out vec4 colour;                                                              \n\
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
-    colour = vec4(1.0, 0.0, 0.0, 1.0);                                        \n\
+    colour = vCol;                                                            \n\
 }";
 
 void CreateTriangle()
 {
+	unsigned int indicies[] = {
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0, 1, 2
+	};
+
 	GLfloat vertices[] = {
 		-1.0f, -1.0f, 0.0f,
+		0.0f, -1.0f, 1.0f, 
 		1.0f, -1.0f, 0.0f,
 		0.0f, 1.0f, 0.0f
 	};
 
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &IBO); 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -72,7 +89,11 @@ void CreateTriangle()
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+	// unbinding VAO
 	glBindVertexArray(0);
+
+	// important: unbind IBO after unbinding VAO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); 
 }
 
 void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
@@ -186,6 +207,9 @@ int main()
 		return 1;
 	}
 
+	// enable depth testing so we know which ones to draw ontop of others
+	glEnable(GL_DEPTH_TEST);
+
 	// Setup Viewport size
 	glViewport(0, 0, bufferWidth, bufferHeight);
 
@@ -236,21 +260,28 @@ int main()
 
 		// Clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(shader);
 
 		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
-		//model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
-		model = glm::scale(model, glm::vec3(curSize, curSize, 1.0f));
+
+		//model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 
 
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model)); 
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,IBO);
+
+
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0); 
+
 		glBindVertexArray(0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); 
 
 		glUseProgram(0);
 
